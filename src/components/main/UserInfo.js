0abdfb2923695824bf926600a31d831a64c4e45d
ysh <shyoo1023@gmail.com>
2024-06-04 12:24:11 +0900

    userinfo - update validation

--- src/components/main/UserInfo.js
+++ src/components/main/UserInfo.js
@@ -1,74 +1,14 @@
 import '../css/UserInfo.scss';
 import HeaderContainer from "../../containers/common/HeaderContainer";
 import Button from '../common/Button';
-import { useState, useEffect, useCallback } from 'react';
 import returnDateString from '../../lib/returnDateString';
-import { useNavigate } from 'react-router-dom';
 import Modal from '../common/Modal';
 
 
-const UserInfo = ({user, updateUserInfo, showModal, modalMsg, closeModal}) => {
+const UserInfo = ({user, updateUserInfo, form, errForm, onChangeForm, onBlurForm, showModal, modalMsg, closeModal, submitUpdate}) => {
     const createdDt = returnDateString(user.createdDt);
 
-    const [form, setForm] = useState(
-        {
-            email: user.email,
-            username: user.username,
-            password: undefined,
-            passConfirm: undefined
-        }
-    );
-
-    const [errForm, setErrForm] = useState(
-        {
-            username: null,
-            password: null,
-            passConfirm: null
-        }
-    );
-
-    const onChangeForm = useCallback(e => {
-        setForm(
-            {
-                ...form,
-                [e.target.name]: e.target.value
-            }
-        )
-    }, [form]);
-
-    const onBlurForm = useCallback(e => {
-        let { name, value } = e.target;
-
-        if(name === 'username'){
-            const username_regex = /^[가-힣a-zA-Z\s]{2,20}$/;
-            let testUsername = username_regex.test(value);
-            if(!testUsername){
-                setErrForm({...errForm, username: '2~20 자의 한글, 영문을 입력해 주세요.'});
-            }else if(testUsername){
-                setErrForm({...errForm, username: null});
-            }
-        }else if(name === 'password'){
-            const password_regex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,16}$/;
-            let testPassword = password_regex.test(value);
-            if(!testPassword && value.length > 0){
-                setErrForm({...errForm, password: '8~16자의 대소문자 및 특수문자를 포함하여 입력해 주세요.'});
-            }else if(testPassword || value.length === 0){
-                setErrForm({...errForm, password: null});
-            }
-
-        }else if(name === 'passConfirm'){
-            let testPassConfirm = form.password === form.passConfirm;
-            if(!testPassConfirm && form.password.length > 0){
-                setErrForm({...errForm, passConfirm: '비밀번호가 일치하지 않습니다.'});
-            }else if(testPassConfirm || value.length === 0){
-                setErrForm({...errForm, passConfirm: null});
-            }
-        }
-    }, [errForm, form]);
-
-    const submitUpdate = () => {
-        updateUserInfo(form);
-    }
+    
     
     return(
         <div className="user_info">
@@ -86,7 +26,7 @@ const UserInfo = ({user, updateUserInfo, showModal, modalMsg, closeModal}) => {
                             </div>
                             <div className="attr_box">
                                 <label>이름</label>
-                                <input type="text" name="username" value={form.username} onChange={onChangeForm} onBlur={onBlurForm} disabled={false}/>
+                                <input type="text" name="username" value={form.username} onChange={onChangeForm} onBlur={onBlurForm} disabled={false} maxLength="20" />
                                 {   errForm.username && 
                                     <div className="err_message">{errForm.username}.</div>
                                 }
@@ -94,14 +34,14 @@ const UserInfo = ({user, updateUserInfo, showModal, modalMsg, closeModal}) => {
                             
                             <div className="attr_box">
                                 <label>비밀번호</label>
-                                <input type="password" name="password" onChange={onChangeForm} onBlur={onBlurForm} />
+                                <input type="password" name="password" onChange={onChangeForm} onBlur={onBlurForm} maxLength="16" />
                                 {   errForm.password && 
                                     <div className="err_message">{errForm.password}</div>
                                 }
                             </div>
                             <div className="attr_box">
                                 <label>비밀번호 확인</label>
-                                <input type="password" name="passConfirm" onChange={onChangeForm} onBlur={onBlurForm} />
+                                <input type="password" name="passConfirm" onChange={onChangeForm} onBlur={onBlurForm} maxLength="16"/>
                                 {   errForm.passConfirm && 
                                     <div className="err_message">{errForm.passConfirm}</div>
                                 }
--- src/containers/home/UserInfoContainer.js
+++ src/containers/home/UserInfoContainer.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import UserInfo from "../../components/main/UserInfo"
 import { useSelector, useDispatch } from "react-redux";
@@ -8,9 +8,17 @@ const UserInfoContainer = () => {
     const dispatch = useDispatch();
     const history = useNavigate();
 
-    const { user } = useSelector(({user}) => ({
-        user: user.user
+    const { user, session } = useSelector(({user, auth}) => ({
+        user: user.user,
+        session: auth.session
     }));
+
+    useEffect(() => {
+        if(!user || !session){
+            history('/login');
+        }
+    }, [user, session, history]);
+
     const { updateMsg, error } = useSelector(({user}) => ({
         updateMsg: user.updateMsg,
         error: user.error
@@ -20,6 +28,37 @@ const UserInfoContainer = () => {
         {
             show: false,
             message: null
+        }
+    );
+
+    useEffect(() => {
+        if(!user){
+            history('/login');
+            return;
+        }
+       
+    }, [user, history, updateMsg, modalOption]);
+
+    const [form, setForm] = useState(
+        user ? 
+        {
+            email: user.email,
+            username: user.username,
+            password: null,
+            passConfirm: null
+        } : {
+            email: null,
+            username: null,
+            password: null,
+            passConfirm: null
+        }
+    );
+    
+    const [errForm, setErrForm] = useState(
+        {
+            username: null,
+            password: null,
+            passConfirm: null
         }
     );
 
@@ -34,17 +73,73 @@ const UserInfoContainer = () => {
             setModalOption({show: true, message: '정보가 수정되었습니다.'});
         }
     };
-    
-    useEffect(() => {
-        if(!user){
-            history('/login');
+
+    const onChangeForm = useCallback(e => {
+        setForm(
+            {
+                ...form,
+                [e.target.name]: e.target.value
+            }
+        )
+    }, [form]);
+
+    const onBlurForm = useCallback(e => {
+        let { name, value } = e.target;
+
+        if(name === 'username'){
+            const username_regex = /^[가-힣a-zA-Z0-9\s]{2,20}$/;
+            let testUsername = username_regex.test(value);
+            if(!testUsername){
+                setErrForm({...errForm, username: '2~20 자의 한글, 영문을 입력해 주세요.'});
+            }else if(testUsername){
+                setErrForm({...errForm, username: null});
+            }
+        }else if(name === 'password'){
+            const password_regex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,16}$/;
+            let testPassword = password_regex.test(value);
+            if(!testPassword && value.length > 0){
+                setErrForm({...errForm, password: '8~16자의 대소문자 및 특수문자를 포함하여 입력해 주세요.'});
+            }else if(testPassword || value.length === 0){
+                setErrForm({...errForm, password: null});
+            }
+
+        }else if(name === 'passConfirm'){
+            let testPassConfirm = form.password === form.passConfirm;
+            if(!testPassConfirm && form.password.length > 0){
+                setErrForm({...errForm, passConfirm: '비밀번호가 일치하지 않습니다.'});
+            }else if(testPassConfirm || value.length === 0){
+                setErrForm({...errForm, passConfirm: null});
+            }
         }
-    }, [user, history, updateMsg, modalOption]);
+    }, [errForm, form]);
+
+    const submitUpdate = () => {
+        if(errForm.username){
+            setModalOption({show: true, message: '이름을 확인해 주세요.'});
+            return;
+        }
+
+        if(errForm.password || errForm.passConfirm){
+            setModalOption({show: true, message: '비밀번호를 확인해 주세요.'});
+            return;
+        }
+        updateUserInfo(form);
+    }
 
     return(
         <div>
             {user &&
-                <UserInfo user={user} updateUserInfo={updateUserInfo} showModal={modalOption.show} modalMsg={modalOption.message} closeModal={closeModal} />
+                <UserInfo 
+                    user={user} 
+                    updateUserInfo={updateUserInfo} 
+                    form={form} 
+                    errForm={errForm} 
+                    onChangeForm={onChangeForm} 
+                    onBlurForm={onBlurForm}
+                    submitUpdate={submitUpdate}
+                    showModal={modalOption.show} 
+                    modalMsg={modalOption.message} 
+                    closeModal={closeModal} />
             }
         </div>
     );
